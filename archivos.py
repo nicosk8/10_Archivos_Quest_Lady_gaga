d0bc66dd110619863b38964d1da259a8016078c9
nicosk8 <nicoledeniseheck@gmail.com>
2025-10-29 11:55:08 -0300

    Se agrega leer_imprimir_archivo() y  validacion por archivo vacio
    Se agrega la funcion leer_imprimir_archivo() y  validacion por archivo vacio.
    
    se agrega la linea:
       encabezado = contenido.pop(0)
    
    Si el archivo viene con encabezado. Se lo elimina asi entran los datos listos para ser procesados ni bien entran a la aplicacion

--- archivos.py
+++ archivos.py
@@ -1,7 +1,50 @@
 #from lady_gaga import playlist_lady_gaga
 
-# Quest Lady Gaga  - funcionciones basicas  ---------------
+# Seccion : nombres y rutas de archivos: 
+CSV_CANCIONES_LADYGAGA = 'C:/Repositorio UTN/2025/PROG I/Practica_Conceptos/10_Archivos_Quest_Lady_gaga/canciones.csv'
+CSV_CANCIONES_LADYGAGA_vacio = 'C:/Repositorio UTN/2025/PROG I/Practica_Conceptos/10_Archivos_Quest_Lady_gaga/canciones_vacio.csv'
+
+# Quest Lady Gaga  - Seccion : funcionciones basicas  ---------------
+
+def obtener_encabezado(cancion: dict) -> str:
+    encabezado = ''
+    for key in cancion.keys():
+        encabezado += f'{key};'
+    encabezado = encabezado[:-1]
+    return encabezado
+
+def parsear_dict_a_csv_str(cancion: dict) -> str:
+    """ Recorre los valores del diccionario, y los concatena en una lista de strings"""
+    info = ''
+    for value in cancion.values():
+        info = f'{info}{value};'
+    # "goku,saiyan,100,50"
+    info = info[:-1]
+    return info
+
+# Validaciones de archivo / contenido leido 
+def es_contenido_vacio(contenido: list, mensaje: str) -> bool:
+    
+    
+    if len(contenido) == 0: 
+        mensaje_error= mensaje
+        print(mensaje_error)
+        contenido_vacio = True
+    else: 
+        contenido_vacio = False
+
+    return contenido_vacio
+        
+
+# Quest Lady Gaga  - Seccion : Apertura de archivos  ---------------
+
+def abrir_archivo(ruta: str, modo = 'r') -> int:
+    """ Abre un archivo que ya existe """
+    archivo = open(ruta, mode= modo , encoding='UTF-8')
+    return archivo
+
 def abrir_escribir_texto(ruta: str, contenido: str, anexar: bool = False) -> int:
+    """ Abre un archivo (si no existe, lo crea), lo graba y lo cierra """
     lineas = 0
 #    modo = 'w'
     modo = 'w+'
@@ -16,27 +59,17 @@ def abrir_escribir_texto(ruta: str, contenido: str, anexar: bool = False) -> int
     else:
         return -1
 
-def obtener_encabezado(cancion: dict) -> str:
-    encabezado = ''
-    for key in cancion.keys():
-        encabezado += f'{key};'
-    encabezado = encabezado[:-1]
-    return encabezado
-
-def parsear_dict_a_csv_str(cancion: dict) -> str:
-    info = ''
-    for value in cancion.values():
-        info = f'{info}{value};'
-    # "goku,saiyan,100,50"
-    info = info[:-1]
-    return info
-
 # Quest Lady Gaga  - Crear archivo CSV -------------------
 
-CSV_CANCIONES_LADYGAGA = 'C:/Repositorio UTN/2025/PROG I/Practica_Conceptos/10_Archivos_Quest_Lady_gaga/canciones.csv'
-
 def crear_archivo_csv(playlist_lady_gaga: list[dict]):
-    """ Crea un archivo a partir de una lista de diccionarios 
+    """ Crea un archivo a partir de una lista de diccionarios.
+        1 - Arma un encabezado con las claves correspondientes a los valores 
+        2 - A partir de la lista de dict cargada en memoria, procesa uno por uno, accediendo
+            a la estructura del diccionario
+        3 - Dentro del proceso llama a una funcion que realiza parseo de dict a lista de strings (un reg. en linea con todos los valores)
+        4 - Llama a funcion para grabar en el archivo, ese registro que tiene cargado en memoria. 
+        Procesa hasta fin de lista de diccionarios.    
+
     :params:
          playlist_lady_gaga -> lista de diccionarios """
     
@@ -52,3 +85,48 @@ def crear_archivo_csv(playlist_lady_gaga: list[dict]):
         abrir_escribir_texto(CSV_CANCIONES_LADYGAGA, contenido= info_canciones, anexar=True)
 
 
+# Quest Lady Gaga  - Seccion : Leer archivo -------------------
+
+def leer_archivo(nombre_archivo):
+    """ Lee un archivo con metodo  .read() """
+    contenido = nombre_archivo.read()
+    return contenido
+
+def leer_archivo_read_lines(nombre_archivo) -> list:
+    """ Lee un archivo con metodo .readlines() , y elimina el salto de linea implicito 
+        que viene por defecto al levantar
+    :params: nombre_archivo -> archivo a leer
+    :returns: lista_contenido -> lista con todos los registros del archivo  """
+    lista_contenido = []
+    lista_contenido = nombre_archivo.readlines()
+
+    for indice_fila in range(len(lista_contenido)):
+        lista_contenido[indice_fila] = lista_contenido[indice_fila].replace('\n', '')
+
+    return lista_contenido
+
+def leer_imprimir_archivo(nombre_archivo):
+    """ 1 - Lee un archivo e imprime su contenido por consola 
+        2 - Valida archivo vacio: Si la lista que carga no està cargada es porque leyò archivo vacìo.
+            2.a - Llama a una funcion que devuelve True si la lista està vacia o False si cargò datos.
+                Si en la vaidacion el resultado es True -> Mostrara msj de error, no ejecutarà nada y tampoco rompe.
+                Si en la vaidacion el resultado es False -> imprimirà por consola lo que cargò desde la lectura.
+                
+        3 - Tambien elimina el encabezado si es que lo trae pero hay que ajustarlo a mano por el momento (LEER COMENTARIO EN VERDE)
+     """
+    
+    contenido = list()
+    archivo = abrir_archivo(nombre_archivo, modo='r')
+
+    contenido = leer_archivo_read_lines(archivo)
+    if not es_contenido_vacio(contenido, mensaje= 'Error en func -> leer_imprimir_archivo() -> El archivo està vacìo'):
+    
+        encabezado = contenido.pop(0) # <- OJO ACA QUE ELIMINA EL PRIMER REG PORQUE VIENE CON ENCABEZADO
+
+        for reg in contenido:
+            print(reg)
+
+
+if __name__ == '__main__':
+#    leer_imprimir_archivo(CSV_CANCIONES_LADYGAGA_vacio)
+    leer_imprimir_archivo(CSV_CANCIONES_LADYGAGA)
